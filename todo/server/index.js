f804d8a088e954f705c121710d961d32776a0285
Juhi Yadav <137876549+yadavjuhi@users.noreply.github.com>
2024-05-13 01:55:01 +0530

    Update index.js

--- todo/server/index.js
+++ todo/server/index.js
@@ -8,7 +8,7 @@ const UserModel = require("./models/Users")
 const app = express()
 app.use(cors({
     origin : ["https://todo-frontend-pied-iota.vercel.app"],
-    methods:["POST","GET"],
+    methods:["POST","GET","PUT","DELETE"],
     credentials:true
 }));
 app.use(express.json())
